2aee407b79d9accb16d0d822c3ed54ce28f72776
Edvinas Baranauskas <edvinas.baranauskas958822@gmail.com>
2021-11-12 21:34:44 +0200

    init project commit

---
+++ NewTest/Examples/Tests/Test.cs
@@ -0,0 +1,39 @@
+﻿using NewTest.Helpers;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace NewTest.Examples.Tests
+{
+    public class Test
+    {
+        private static readonly IWebDriver Driver = SeleniumHelpers.Driver;
+
+        [OneTimeSetUp]
+
+        public void SetupTests()
+        {
+            if (SeleniumHelpers.Driver == null)
+            {
+                SeleniumHelpers.Driver = new ChromeDriver();
+            }
+        }
+
+        [OneTimeTearDown]
+
+        public void TearDown()
+        {
+            if (SeleniumHelpers.Driver != null)
+            {
+                SeleniumHelpers.Driver.Quit();
+            }
+        }
+
+        [Test]
+        public void CheckTitle()
+        {
+            Class1.GoToPage();
+            Assert.AreEqual(Class1.GetText(), "Next steps");
+        }
+    }
+}
